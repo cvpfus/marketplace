089149f3ef6b8441c7b68b9fc8ab5d6fae48ba80
Yusuf Praditya <47532266+yusufpraditya@users.noreply.github.com>
2024-12-23 19:57:03 +0700

    fix: fix null error

--- frontend/src/hooks/use-get-user.js
+++ frontend/src/hooks/use-get-user.js
@@ -6,7 +6,7 @@ export const useGetUser = () => {
   const { item } = useLocalStorage("marketplace");
 
   return useQuery({
-    queryKey: [`user-${item.userRecordId}`],
+    queryKey: [`user-${item?.userRecordId}`],
     queryFn: () => getUser({ userId: item.userRecordId, token: item.token }),
     enabled: !!item,
   });
